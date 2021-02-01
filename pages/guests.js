b4a3e84891531af29d7b2ce4af31113eccb56cee
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-02-01 18:52:11 +0900

    add guests page

--- pages/guests.js
+++ pages/guests.js
@@ -1,31 +1,39 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
 import Header from '../components/header'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/graphcms'
+import { getAllGuests } from '../lib/graphcms'
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Index({ posts, preview }) {
-  const heroPost = posts[0]
-  const morePosts = posts.slice(1)
+export default function Guests({ guests }) {
   return (
     <>
-      <Layout preview={preview}>
         <Head>
           <title>Guests</title>
         </Head>
         <Container>
           <Header />
-          <h2>Guest Page</h2>
+          <section className="grid grid-cols-1 md:grid-cols-4 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-25 mb-32">
+          {guests.map((guest) => (
+             <>
+               <div>
+               <img src={guest.picture.url} className="w-auto h-auto mb-5 border-4 border-black rounded-full" alt={guest.name} />
+               <h3 className="mb-3 text-2xl text-center leading-snug">
+                <Link as={`/guests/${guest.slug}`} href="/guests/[guest.slug]">
+                  <a className="hover:underline">{guest.name}</a>
+                </Link>
+               </h3>
+               </div>
+             </>
+          ))}
+          </section>
         </Container>
-      </Layout>
     </>
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const posts = (await getAllPosts(preview)) || []
+export async function getStaticProps() {
+  const guests = await getAllGuests()
   return {
-    props: { posts, preview },
+    props: { guests },
   }
 }
--- pages/guests/[slug].js
+++ pages/guests/[slug].js
@@ -1,73 +1,35 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
 import Container from 'components/container'
-import PostBody from 'components/post-body'
-import MoreStories from 'components/more-stories'
-import Header from 'components/header'
-import PostHeader from 'components/post-header'
-import SectionSeparator from 'components/section-separator'
 import Layout from 'components/layout'
-import { getAllPostsWithSlug, getPostAndMorePosts } from 'lib/graphcms'
-import PostTitle from 'components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from 'lib/constants'
+import Header from 'components/header'
+import { getGuest, getAllGuestsWithSlug } from 'lib/graphcms'
 
-export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-
+export default function Guest({ guest }) {
   return (
-    <Layout preview={preview}>
+    <Layout>
       <Container>
         <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article>
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                {/* <meta property="og:image" content={post.ogImage.url} /> */}
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-            <SectionSeparator />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-          </>
-        )}
+        <>
+        <p>{guest.name}</p>
+        <p>{guest.biography}</p>
+        </>
       </Container>
     </Layout>
   )
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  const data = await getPostAndMorePosts(params.slug, preview)
+export async function getStaticPaths() {
+  const guests = await getAllGuestsWithSlug()
   return {
-    props: {
-      preview,
-      post: data.post,
-      morePosts: data.morePosts || [],
-    },
+    paths: guests.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
   }
 }
 
-export async function getStaticPaths() {
-  const posts = await getAllPostsWithSlug()
+export async function getStaticProps({ params }) {
+  const data = await getGuest(params.slug)
   return {
-    paths: posts.map(({ slug }) => ({
-      params: { slug },
-    })),
-    fallback: true,
+    props: { guest:data.guest }
   }
 }
