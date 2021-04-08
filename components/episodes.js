1ea4aa316588033e60f70d792966c534cfae566f
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-04-08 16:41:54 +0900

    fix bg view height

--- components/episodes.js
+++ components/episodes.js
@@ -5,7 +5,7 @@ export default function MoreStories({ episodes }) {
     <div className="w-3/4">
       {episodes.map((episode) => (
         <Link as={`/episodes/${episode.slug}`} href="/episodes/[episode.slug]">
-          <div className="p-6 mb-10 bg-white rounded-xl shadow-lg">
+          <div className="p-6 mb-10 bg-white rounded-xl">
             <p className="text-xs">{episode.date}</p>
             <h3 className="mb-3 text-2xl leading-snug">{episode.title}</h3>
             <p>{episode.description}</p>
--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,12 @@ export default function Layout({ preview, children }) {
     <>
       <Meta />
       <div className="min-h-screen">
-        <main>{children}</main>
+        <main>
+          <div className="relative z-0 bg-yellow-50 pb-20 min-h-screen">
+            <div className="absolute top-0 left-0 right-0 -z-10 bg-wave"></div>
+            {children}
+          </div>
+        </main>
       </div>
       <Footer />
     </>
