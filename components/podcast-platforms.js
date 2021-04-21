518e7bd66eeda168f7d63ed663a319d5e87ee691
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-04-21 18:17:14 +0900

    add platform url

--- components/podcast-platforms.js
+++ components/podcast-platforms.js
@@ -1,10 +1,10 @@
 const platform = [
-  { href: "/", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/apple-podcasts_uq7puz.png", label: "Apple Podcast" },
-  { href: "/posts", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/spotify_nevf2a.png", label: "Spotify" },
-  { href: "/posts", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/stand-fm_sg6jjk.png", label: "stand.fm" },
-  { href: "/posts", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/google-podcasts_bdjuud.png", label: "Google Podcasts" },
-  { href: "/posts", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/pocketcasts_pntxu4.png", label: "Pocket Cast" },
-  { href: "/posts", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/rss_x7j0t3.png", label: "RSS" },
+  { href: "https://podcasts.apple.com/jp/podcast/ダイアログラジオ-dialogue-radio/id1527150714?l=en", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/apple-podcasts_uq7puz.png", label: "Apple Podcast" },
+  { href: "https://open.spotify.com/show/1HXmRr01UCE3NFz5faNdiP", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/spotify_nevf2a.png", label: "Spotify" },
+  { href: "https://stand.fm/channels/5ed5258a354d77a05016ae9a", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/stand-fm_sg6jjk.png", label: "stand.fm" },
+  { href: "https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy8zMDc3NTYyMC9wb2RjYXN0L3Jzcw==", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/google-podcasts_bdjuud.png", label: "Google Podcasts" },
+  { href: "https://pca.st/2hrg9jti", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/pocketcasts_pntxu4.png", label: "Pocket Cast" },
+  { href: "https://anchor.fm/s/30775620/podcast/rss", src: "https://res.cloudinary.com/dialogue-radio/image/upload/v1617787619/icon/rss_x7j0t3.png", label: "RSS" },
 ]
 
 export default function PodcastPlatforms() {
@@ -16,7 +16,7 @@ export default function PodcastPlatforms() {
       <ul>
       {platform.map(({ href, src, label }) => (
           <li className="w-full py-2 text-white mt-5 md:w-auto md:py-auto">
-            <a href={href}>
+            <a href={href} target="_blank">
               <div className="inline-flex items-center justify-center">
                 <div className="w-7 h-auto mr-4">
                   <img className="" src={src}></img>
