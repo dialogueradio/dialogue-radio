3716ca4d77cf02aeceb29ef3774c01b7ed909f89
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-04-25 18:15:01 +0900

    sp hero message

--- components/hero-message.js
+++ components/hero-message.js
@@ -1,9 +1,16 @@
 export default function HeroHome() {
   return (
-    <div className="w-auto mx-auto mt-10 mb-12 text-center md:w-min">
-      <h2 className="w-auto inline-block text-primary bg-white mb-3 text-2xl font-bold py-2 px-2 md:w-max">ダイアログラジオとは？</h2>
-      <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 md:w-max">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで</p>
-      <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 mt-2 md:w-max">多様な考え方、働き方、生き方の発信を目指しています</p>
-    </div>
+    <>
+      <div className="hidden w-auto mx-auto mt-10 mb-12 text-center md:block w-min">
+        <h2 className="w-auto inline-block text-primary bg-white mb-3 text-2xl font-bold py-2 px-2 md:w-max">ダイアログラジオとは？</h2>
+        <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 md:w-max">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで</p>
+        <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 mt-2 md:w-max">多様な考え方、働き方、生き方の発信を目指しています</p>
+      </div>
+
+      <div className="block text-left w-auto mx-auto md:hidden">
+        <h2 className="w-auto inline-block text-primary bg-white mb-3 text-base font-bold py-2 px-2 md:w-max">ダイアログラジオとは？</h2>
+        <p className="w-auto inline-block text-primary bg-white text-xs font-bold py-0.5 px-2">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで多様な考え方、働き方、生き方の発信を目指しています</p>
+      </div>
+    </>
   ) 
 }
