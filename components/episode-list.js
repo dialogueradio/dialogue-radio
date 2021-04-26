022d568bd910826ee1ba915ac101a45352ff55ab
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-04-26 18:16:53 +0900

    fix layout episdoe list

--- components/episode-list.js
+++ components/episode-list.js
@@ -8,8 +8,8 @@ export default function Episode({ episodes }) {
       {episodes.map((episode) => (
         <Link as={`/episodes/${episode.slug}`} href="/episodes/[episode.slug]">
           <a>
-            <div className="py-5 pb-6 px-4 md:p-6 mb-6 md:mb-10 bg-white rounded-lg md:rounded-xl md:flex justify-between">
-              <div>
+            <div className="py-5 pb-6 px-4 md:p-6 mb-6 md:mb-10 bg-white rounded-lg md:rounded-xl md:flex md:justify-between">
+              <div className="md:w-4/6">
                 <p className="text-xs md:text-base font-bold text-gray3">{episode.date}</p>
                 <div className="flex items-center mt-2 mb-3">
                   <div className="mr-2 md:hidden">
@@ -19,8 +19,8 @@ export default function Episode({ episodes }) {
                 </div>
                 <p className="text-sm md:text-base float-left w-3/4 mr-2 md:float-none lg:w-auto">{episode.description}</p>
               </div>
-              <div>
-                <div className="flex items-end">
+              <div className="md:w-2/6">
+                <div className="flex justify-end items-end">
                   <div className="hidden md:flex mr-12 mb-2">
                     <ResumeButton />
                   </div>
