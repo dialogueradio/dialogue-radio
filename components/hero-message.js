20f204bb155ffb009ed831c26d9c63478dfbb18e
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-05-17 19:17:31 +0900

    add SNS links

--- components/hero-message.js
+++ components/hero-message.js
@@ -1,15 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 export default function HeroMessage() {
   return (
     <>
-      <div className="hidden w-auto mx-auto mt-10 mb-12 text-center md:block w-min">
-        <h2 className="w-auto inline-block text-primary bg-white mb-3 text-2xl font-bold py-2 px-2 md:w-max">ダイアログラジオとは？</h2>
-        <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 md:w-max">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで</p>
-        <p className="w-auto inline-block text-primary bg-white text-lg font-bold py-0.5 px-2 mt-2 md:w-max">多様な考え方、働き方、生き方の発信を目指しています</p>
-      </div>
-
-      <div className="block text-left w-auto mx-auto md:hidden">
-        <h2 className="w-auto inline-block text-primary bg-white mb-3 text-base font-bold py-2 px-2 md:w-max">ダイアログラジオとは？</h2>
-        <p className="w-auto inline-block text-primary bg-white text-sm font-bold py-0.5 px-2">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで多様な考え方、働き方、生き方の発信を目指しています</p>
+      <div className="w-auto mx-auto md:w-min md:mb-12">
+        <h2 className="w-auto inline-block text-primary bg-white mb-3 text-base font-bold py-2 px-5 md:text-2xl md:w-max">ダイアログラジオとは？</h2>
+        <div className="flex items-center">
+          <p className="w-auto inline-block text-primary bg-white text-sm font-bold py-2 px-3 md:w-max md:max-w-xl md:text-lg md:leading-6">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで多様な考え方、働き方、生き方の発信を目指しています</p>
+          <div className="pl-10 hidden md:flex">
+            <a href="https://www.facebook.com/DialogueRadio2020/" target="_blank">
+              <span className="flex justify-center items-center w-12 h-12 bg-white bg-opacity-25 rounded-full mr-5 border-2 duration-100 border-transparent hover:border-white">
+                <FontAwesomeIcon icon={faFacebookF} className="text-white w-2.5"/>
+              </span>
+            </a>
+            <a href="https://www.instagram.com/dialogue_radio/" target="_blank">
+              <span className="flex justify-center items-center w-12 h-12 bg-white bg-opacity-25 rounded-full border-2 duration-100 border-transparent hover:border-white">
+                <FontAwesomeIcon icon={faInstagram} className="text-white h-5"/>
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     </>
   ) 
--- components/resume-button.js
+++ components/resume-button.js
@@ -1,6 +1,6 @@
 export default function ReumeButton() {
   return (
-      <div className={`w-14 h-14 rounded-full flex items-center justify-center bg-accent shadow-lg`}>
+      <div className={`w-14 h-14 rounded-full flex items-center justify-center bg-black shadow-lg`}>
         <img
         src="https://res.cloudinary.com/dialogue-radio/image/upload/v1617956066/triangle_hlz6c8.svg" 
         className={`w-5 h-auto ml-1`}
