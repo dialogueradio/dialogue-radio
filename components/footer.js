0efdecb2f275688e7f57e6955336c81099670516
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-05-26 18:42:04 +0900

    add sns button

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,19 @@
 import Container from './container'
+import SnsButton from './sns-button'
 import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Footer() {
   return (
-    <footer className="bg-yellow-100">
+    <footer className="">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-14 flex justify-between flex-col lg:flex-row items-center">
+          <a
+            href="https://forms.gle/QtJQ4iXsFish9BiD7"
+            target="_blank"
+            className="px-5 py-4 bg-black text-white font-bold rounded-md">
+            お便り・お問い合わせ
+          </a>
+          <SnsButton />
         </div>
       </Container>
     </footer>
--- components/hero-message.js
+++ components/hero-message.js
@@ -1,6 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import SnsButton from './sns-button'
 
 export default function HeroMessage() {
   return (
@@ -10,16 +8,7 @@ export default function HeroMessage() {
         <div className="flex items-center">
           <p className="w-auto inline-block text-primary bg-white text-sm font-bold py-2 px-3 md:w-max md:max-w-xl md:text-lg md:leading-6">ゲストと私たちパーソナリティーが、人生について多方面からカジュアルに語ることで多様な考え方、働き方、生き方の発信を目指しています</p>
           <div className="pl-10 hidden md:flex">
-            <a href="https://www.facebook.com/DialogueRadio2020/" target="_blank">
-              <span className="flex justify-center items-center w-12 h-12 bg-white bg-opacity-25 rounded-full mr-5 border-2 duration-100 border-transparent hover:border-white">
-                <FontAwesomeIcon icon={faFacebookF} className="text-white w-2.5"/>
-              </span>
-            </a>
-            <a href="https://www.instagram.com/dialogue_radio/" target="_blank">
-              <span className="flex justify-center items-center w-12 h-12 bg-white bg-opacity-25 rounded-full border-2 duration-100 border-transparent hover:border-white">
-                <FontAwesomeIcon icon={faInstagram} className="text-white h-5"/>
-              </span>
-            </a>
+          <SnsButton variant={"hero"} />
           </div>
         </div>
       </div>
