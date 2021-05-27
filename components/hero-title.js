33cde6694f68a448626e7d08a271bd772ed3a55e
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-05-28 04:42:42 +0900

    style small fix

--- components/hero-title.js
+++ components/hero-title.js
@@ -1,7 +1,7 @@
 export default function HeroSubpage({ title }) {
   return (
     <div className="mb-16 md:ml-20 text-center md:text-left">
-      <h1 className="text-4xl md:text-6xl font-bold text-white d-underline pr-2 ">{title}</h1>
+      <h1 className="text-4xl md:text-6xl font-bold text-white pr-2 d-underline">{title}</h1>
     </div>
   ) 
 }
--- components/sns-button.js
+++ components/sns-button.js
@@ -15,7 +15,7 @@ export default function SnsButton({variant}) {
     { href: "https://www.instagram.com/dialogue_radio/", element: instagram},
   ]
   return (
-    <div className="flex items-center justify-between w-28 mr-0 md:mr-6">
+    <div className="flex items-center justify-between w-28 md:mr-6">
       {links.map(({ href, element }) => (
         <a href={href} target="_blank">
           <span className={`group flex justify-center items-center w-12 h-12 rounded-full border-2 duration-100 border-transparent ${ variant ? "bg-white bg-opacity-40  hover:border-white" : "border-black hover:border-primary"}`}>
