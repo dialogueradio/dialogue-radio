cded1663f02bc8b674a44b07e69852fff1623199
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-06-06 19:41:13 +0900

    add google analytics

--- components/meta.js
+++ components/meta.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { existsGaId, GA_ID } from '../lib/gtag'
 import { HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
@@ -28,6 +29,23 @@ export default function Meta() {
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      {/* Google Analytics */}
+      {existsGaId && (
+        <>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });`,
+            }}
+          />
+        </>
+      )}
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import '../styles/index.css'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    if (!gtag.existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      gtag.pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
