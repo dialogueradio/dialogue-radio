e38cb1d1cf1152b5cf97314167b92217fedf09dc
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2021-07-01 21:13:02 +0900

    fix layout from horizontal to vertical

--- components/episode-related-posts.js
+++ components/episode-related-posts.js
@@ -5,17 +5,17 @@ export default function EpisodeHostGuest({ relatedPosts }) {
   return (
     <div>
       {relatedPosts.map((relatedPost) => (
-        <div className="flex">
-          <div className="w-2/4">
+        <div className="md:flex">
+          <div className="md:w-2/4">
             <CoverImage slug={relatedPost.slug} title={relatedPost.title} url={relatedPost.coverImage.url} />
           </div>
-          <div className="w-3/4 ml-4 md:ml-7">
-            <h3 className="md:my-2 text-base md:text-xl font-bold leading-snug">
+          <div className="mt-3 md:w-3/4 md:ml-4 md:ml-7 md:mt-1">
+            <h3 className="text-base font-bold leading-snug md:mb-2 md:text-xl">
               <Link as={`/posts/${relatedPost.slug}`} href="/posts/[slug]">
                 <a className="hover:underline">{relatedPost.title}</a>
               </Link>
             </h3>
-            <p className="mb-4 text-sm md:text-base leading-relaxed">{relatedPost.excerpt}</p>
+            <p className="mb-4 text-sm leading-relaxed md:text-base">{relatedPost.excerpt}</p>
           </div>
         </div>
       ))}
